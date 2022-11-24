05486a66ff7a4d15a6ba8fa25c02b35849d035ef
felixokolo <pheelix.info@gmail.com>
2022-11-24 17:12:02 +0100

    Incomlete implementation of POST

--- web/api/v1/views/users.py
+++ web/api/v1/views/users.py
@@ -14,14 +14,17 @@ def hello_world():
 
 @app_views.route("/<uname>", methods=['GET', 'POST'])
 def get_user_nodes(uname):
-    locs = User.get_locs(uname)
-    if locs is None:
+    if request.method == 'GET':
+        locs = User.get_locs(uname)
+        if locs is None:
+            return jsonify(None)
+        nodes = []
+        for loc in locs:
+            loc_node = Location.get_nodes(loc.id)
+            if loc_node is not None:
+                nodes += loc_node
+        if nodes is not None:
+            return jsonify([node.to_dict() for node in nodes])
         return jsonify(None)
-    nodes = []
-    for loc in locs:
-        loc_node = Location.get_nodes(loc.id)
-        if loc_node is not None:
-            nodes += loc_node
-    if nodes is not None:
-        return jsonify([node.to_dict() for node in nodes])
-    return jsonify(None)
+
+    if request.method == 'POST':
