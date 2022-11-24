951937cb194f281c2a0f5cb78e47864be11d4849
felixokolo <pheelix.info@gmail.com>
2022-11-24 14:26:50 +0100

    Created a blueprint

--- web/api/v1/app.py
+++ web/api/v1/app.py
@@ -1,36 +1,10 @@
 #!/usr/bin/python3
 """Web server app"""
 from flask import Flask
-from markupsafe import escape
-from web.models import storage
-from web.models.user import User
-from web.models.location  import Location
-from flask.json import jsonify
+from web.api.v1.views import app_views
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/<name>")
-def hello_name(name):
-    return f"Hello, {name}!"
-
-@app.route("/agile/<uname>")
-def get_user_nodes(uname):
-    locs = User.get_locs(uname)
-    if locs is None:
-        return jsonify(None)
-    nodes = []
-    for loc in locs:
-        loc_node = Location.get_nodes(loc.id)
-        if loc_node is not None:
-            nodes += loc_node
-    if nodes is not None:
-        return jsonify([node.to_dict() for node in nodes])
-    return jsonify(None)
-
+app.register_blueprint(app_views)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
---
+++ web/api/v1/views/users.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Blueprint for API """
+from web.api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+from markupsafe import escape
+from web.models import storage
+from web.models.user import User
+from web.models.location  import Location
+
+@app_views.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app_views.route("/<uname>", methods=['GET', 'POST'])
+def get_user_nodes(uname):
+    locs = User.get_locs(uname)
+    if locs is None:
+        return jsonify(None)
+    nodes = []
+    for loc in locs:
+        loc_node = Location.get_nodes(loc.id)
+        if loc_node is not None:
+            nodes += loc_node
+    if nodes is not None:
+        return jsonify([node.to_dict() for node in nodes])
+    return jsonify(None)
