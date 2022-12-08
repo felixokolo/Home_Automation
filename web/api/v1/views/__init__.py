d978ae023f7c4e9021dea9478d9347c04d9c0a06
felixokolo <pheelix.info@gmail.com>
2022-12-08 15:08:38 +0100

    change to port 5003

--- web/api/v1/views/__init__.py
+++ web/api/v1/views/__init__.py
@@ -2,6 +2,6 @@
 """ Blueprint for API """
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/agile')
+app_views = Blueprint('app_views', __name__, url_prefix='/agile/api/v1')
 
 from web.api.v1.views import users
